136f914877f4ee0e060b2cca519f36a88a78060b
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-02 23:29:12 +0100

    feat: add default app JSON response body type definition

---
+++ backend/src/common/types/response.type.ts
@@ -0,0 +1,13 @@
+import { Response } from "express";
+
+type TDefaultData = Record<string, unknown>;
+
+type TDefaultResponseBody<TData = TDefaultData> = {
+    status: number;
+    message: string;
+    data: TData;
+};
+
+type TResponse<ResponseBody = TDefaultResponseBody> = Response<ResponseBody>;
+
+export default TResponse;
