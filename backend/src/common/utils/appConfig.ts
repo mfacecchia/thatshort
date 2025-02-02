4000fb1bc63a6d94b8c99154a840ca10508a3564
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-02 23:35:43 +0100

    feat: add app startup utility functions

---
+++ backend/src/common/utils/appConfig.ts
@@ -0,0 +1,39 @@
+import TResponse from "common/types/response.type";
+import cors from "cors";
+import express, { Application, json } from "express";
+import { BASE_V1_ENDPOINT } from "./constants";
+import { setJsonResponse } from "./setJsonResponse";
+
+// TODO: Make this a class with all relative methods
+function startup(): Application {
+    const app = express();
+    app.set("x-powered-by", false);
+    app.use(
+        cors({
+            origin: process.env.FRONTEND_ADDRESS,
+        })
+    );
+    app.use(json());
+
+    buildDebugEndpoint(app);
+    buildNotFoundEndpoint(app);
+    return app;
+}
+
+function buildDebugEndpoint(app: Application) {
+    if (process.env.NODE_ENV === "production") return;
+    app.get(BASE_V1_ENDPOINT + "/debug", (req, res: TResponse) => {
+        setJsonResponse(res, 200, "Success", {});
+        return;
+    });
+}
+
+function buildNotFoundEndpoint(app: Application) {
+    app.use((req, res: TResponse) => {
+        res.status(404).json({
+            status: 404,
+            message: `Route ${req.baseUrl + req.path} not found.`,
+            data: {},
+        });
+    });
+}
