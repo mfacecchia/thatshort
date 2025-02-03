b21ffa49e378c918cbf4e611df9e17db72cc7913
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 22:14:56 +0100

    refactor: update express handler type definition

--- backend/src/common/types/request.type.ts
+++ backend/src/common/types/request.type.ts
@@ -1,6 +1,6 @@
-import { Request } from "express";
+import { NextFunction, Request } from "express";
 import TResponse from "./response.type";
 
-type THandler = (req: Request, res: TResponse) => void;
+type THandler = (req: Request, res: TResponse, next: NextFunction) => void;
 
 export { THandler };
