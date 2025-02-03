dd340238518c2c667179dd26ad48dc9c979318d3
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 20:06:34 +0100

    feat(page): add 404 page

---
+++ frontend/src/common/pages/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import notFound from "@/common/assets/notFound.svg";
+import BackHomeButton from "../components/backHomeButton";
+
+const NotFoundPage = () => {
+    return (
+        <div className="space-y-11">
+            <div className="flex flex-col items-center justify-center w-full">
+                <img src={notFound} alt="Not Found" className="size-48" />
+                <div className="text-center space-y-4 max-w-80">
+                    <h1 className="leading-none">Well... That's embarassing</h1>
+                    <p className="large">
+                        We could not find what you were looking for :(
+                    </p>
+                    <BackHomeButton />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
