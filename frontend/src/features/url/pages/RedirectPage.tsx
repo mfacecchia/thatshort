8591ab4171705d767550cc11f3e4e0c4137e40f7
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 20:08:13 +0100

    feat: display optional message in case of invalid link provided

--- frontend/src/features/url/pages/RedirectPage.tsx
+++ frontend/src/features/url/pages/RedirectPage.tsx
@@ -1,4 +1,6 @@
+import invalidLink from "@/common/assets/invalidLink.svg";
 import redirecting from "@/common/assets/redirecting.svg";
+import BackHomeButton from "@/common/components/backHomeButton";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
@@ -7,20 +9,23 @@ const RedirectPage = () => {
     const params = useParams();
     const { id } = params;
     const [uselessFact, setUselessFact] = useState<string | null>(null);
-
-    axios
-        .get(import.meta.env.VITE_BACKEND_ADDRESS + "/api/v1/url/" + id)
-        .then(({ data: { data } }) => {
-            setTimeout(() => {
-                window.location.href = data.redirect_to;
-            }, 250);
-        })
-        .catch((err) => {
-            // TODO: Display error message on link not found
-            console.error(err);
-        });
+    const [isRedirecting, setIsRedirecting] = useState<boolean>(true);
 
     useEffect(() => {
+        axios
+            .get(import.meta.env.VITE_BACKEND_ADDRESS + "/api/v1/url/" + id)
+            .then(({ data: { data } }) => {
+                setTimeout(() => {
+                    window.location.href = data.redirect_to;
+                }, 250);
+            })
+            .catch(() => {
+                setIsRedirecting(false);
+            });
+    }, [id]);
+
+    useEffect(() => {
+        if (!isRedirecting) return;
         axios
             .get("https://uselessfacts.jsph.pl/random.json")
             .then(({ data: { text } }) => {
@@ -31,23 +36,53 @@ const RedirectPage = () => {
                     "When Google entered the phone market with its Android devices, Steve Jobs felt betrayed and stated that they had stolen some features of the iPhone."
                 );
             });
-    }, []);
+    }, [isRedirecting]);
 
     return (
         <div className="space-y-11">
             <div className="flex flex-col items-center justify-center w-full">
-                <img src={redirecting} alt="Redirecting" className="size-48" />
+                {isRedirecting ? (
+                    <img
+                        src={redirecting}
+                        alt="Redirecting"
+                        className="size-48"
+                    />
+                ) : (
+                    <img
+                        src={invalidLink}
+                        alt="Invalid Link"
+                        className="size-48"
+                    />
+                )}
                 <div className="text-center space-y-4 max-w-80">
-                    <h1 className="leading-none">
-                        You're about to be redirected!
-                    </h1>
-                    <div className="space-y-1">
-                        <p className="large leading-none">Did you know?</p>
-                        {uselessFact && <p>{uselessFact}</p>}
-                    </div>
+                    {isRedirecting ? (
+                        <>
+                            <h1 className="leading-none">
+                                You're about to be redirected!
+                            </h1>
+                            <div className="space-y-1">
+                                <p className="large leading-none">
+                                    Did you know?
+                                </p>
+                                {uselessFact && <p>{uselessFact}</p>}
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <h1 className="leading-none">
+                                Well... That's embarassing
+                            </h1>
+                            <div className="space-y-1">
+                                <p className="large leading-none">
+                                    We could not find the link in our systems
+                                    but here’s a bag of fries just for you :) 🍟
+                                </p>
+                            </div>
+                            <BackHomeButton />
+                        </>
+                    )}
                 </div>
             </div>
-            <div className="w-full"></div>
         </div>
     );
 };
