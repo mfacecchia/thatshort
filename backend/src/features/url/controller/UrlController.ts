b4877a91c6367812db242c98332bf0f217673777
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 22:14:13 +0100

    feat: wrap controllers in try/catch blocks

--- backend/src/features/url/controller/UrlController.ts
+++ backend/src/features/url/controller/UrlController.ts
@@ -1,5 +1,5 @@
 import { url } from "@prisma/client";
-import { Application, Request } from "express";
+import { Application, NextFunction, Request } from "express";
 import { THandler } from "../../../common/types/request.type";
 import TResponse from "../../../common/types/response.type";
 import { BASE_V1_ENDPOINT } from "../../../common/utils/constants";
@@ -21,24 +21,34 @@ class UrlController {
     }
 
     findUrl(): THandler {
-        return async (req: Request, res: TResponse) => {
-            let urlId = req.params["urlId"];
-            let url = await this.urlService.findUrl(urlId);
-            setJsonResponse(res, 200, "Success", url);
-            return;
+        return async (req: Request, res: TResponse, next: NextFunction) => {
+            try {
+                let urlId = req.params["urlId"];
+                let url = await this.urlService.findUrl(urlId);
+                setJsonResponse(res, 200, "Success", url);
+                return;
+            } catch (err) {
+                next(err);
+                return;
+            }
         };
     }
 
     createUrl(): THandler {
-        return async (req: Request, res: TResponse) => {
-            let url: url = req.body;
-            url.id = this.urlService.generateUrlId();
-            await this.urlService.createUrl(url);
-            let shortenedUrl = this.urlService.generateShortenedUrl(url);
-            setJsonResponse(res, 201, "Success", {
-                shortenedUrl: shortenedUrl,
-            });
-            return;
+        return async (req: Request, res: TResponse, next: NextFunction) => {
+            try {
+                let url: url = req.body;
+                url.id = this.urlService.generateUrlId();
+                await this.urlService.createUrl(url);
+                let shortenedUrl = this.urlService.generateShortenedUrl(url);
+                setJsonResponse(res, 201, "Success", {
+                    shortenedUrl: shortenedUrl,
+                });
+                return;
+            } catch (err) {
+                next(err);
+                return;
+            }
         };
     }
 }
