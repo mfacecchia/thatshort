83a734f00fc162ef9a626dced26281b9d3cdc067
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 16:50:55 +0100

    feat(component): add footer component

---
+++ frontend/src/common/components/footer.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+    return (
+        <footer>
+            <span className="small">
+                Developed with
+                <svg
+                    className="size-4 inline-block align-text-bottom hover:animate-ping mx-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2.4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </svg>
+                and a MacBook by Marco Facecchia
+            </span>
+        </footer>
+    );
+};
+
+export default Footer;
