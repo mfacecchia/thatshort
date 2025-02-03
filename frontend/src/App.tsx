a67b1e07a172d6da6c43b3028b681a40e60823ff
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 16:54:15 +0100

    feat(routing): add browser routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Route, Routes } from "react-router";
+import LandingPage from "./features/landing/LandingPage";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<LandingPage />} />
+                </Route>
+                {/* TODO: Add 404 page */}
+                <Route path="*" element={<h1>404</h1>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
