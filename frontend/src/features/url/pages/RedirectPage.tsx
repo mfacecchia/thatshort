f4223c5dac2cacb108a0d688cd88564ea20ed9b5
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 19:30:58 +0100

    feat: add redirecting page components

--- frontend/src/features/url/pages/RedirectPage.tsx
+++ frontend/src/features/url/pages/RedirectPage.tsx
@@ -1,3 +1,4 @@
+import redirecting from "@/common/assets/redirecting.svg";
 import axios from "axios";
 import { useParams } from "react-router";
 
@@ -18,9 +19,20 @@ const RedirectPage = () => {
         });
 
     return (
-        <>
-            <h1>Redirecting...</h1>
-        </>
+        <div className="space-y-11">
+            <div className="flex flex-col items-center justify-center w-full">
+                <img src={redirecting} alt="Redirecting" className="size-48" />
+                <div className="text-center space-y-4 max-w-80">
+                    <h1 className="leading-none">
+                        You're about to be redirected!
+                    </h1>
+                    <div className="space-y-1">
+                        <p className="large leading-none">Did you know?</p>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full"></div>
+        </div>
     );
 };
 
