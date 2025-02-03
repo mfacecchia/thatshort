27e6599f7e66668cebeb4259bd9a074c74907ae2
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-03 11:59:47 +0100

    feat: add url service layer class

---
+++ backend/src/features/url/service/UrlService.ts
@@ -0,0 +1,40 @@
+import { url } from "@prisma/client";
+import crypto from "crypto";
+import UrlRepository from "../repository/UrlRepository";
+
+class UrlService {
+    private urlRepository: UrlRepository;
+
+    constructor() {
+        this.urlRepository = new UrlRepository();
+    }
+
+    async findUrl(id: url["id"]): Promise<url> {
+        try {
+            let url = await this.urlRepository.find(id);
+            if (!url) {
+                // TODO: Throw custom error
+                throw new Error("Url not found.");
+            }
+            return url;
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    }
+
+    async createUrl(url: url): Promise<void> {
+        try {
+            await this.urlRepository.create(url);
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    }
+
+    generateUrlId(): string {
+        return crypto.randomBytes(4).toString("hex");
+    }
+}
+
+export default UrlService;
