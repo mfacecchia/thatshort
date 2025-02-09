1c281b1a398a19a74eea929c54e1817b57aef966
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-09 11:21:15 +0100

    feat: add url generation/validation rate limits

---
+++ backend/src/common/utils/rateLimits.ts
@@ -0,0 +1,53 @@
+import { Request } from "express";
+import rateLimit from "express-rate-limit";
+import requestIp from "request-ip";
+import TResponse from "../types/response.type";
+import { setJsonResponse } from "./setJsonResponse";
+
+const tooManyRequestsHandler = async (req: Request, res: TResponse) => {
+    const retryAfter = res.getHeader("retry-after");
+    return setJsonResponse(
+        res,
+        429,
+        `Too many requests. Please try again in ${
+            retryAfter ? retryAfter + " seconds" : "a few minutes"
+        }.`,
+        {}
+    );
+};
+
+const urlGenerationRateLimit = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 3,
+    standardHeaders: "draft-6",
+    keyGenerator: (req: Request, res: TResponse) => {
+        const ip = requestIp.getClientIp(req);
+        if (!ip) return req.ip || "127.0.0.1";
+        return ip;
+    },
+    message: async (req: Request, res: TResponse) => {
+        const retryAfter = res.getHeader("retry-after");
+        return setJsonResponse(
+            res,
+            429,
+            `Too many requests. Please try again in ${
+                retryAfter ? retryAfter + " seconds" : "a few minutes"
+            }.`,
+            {}
+        );
+    },
+});
+
+const urlRedirectRateLimit = rateLimit({
+    windowMs: 2 * 60 * 1000,
+    limit: 30,
+    standardHeaders: "draft-6",
+    keyGenerator: (req: Request, res: TResponse) => {
+        const ip = requestIp.getClientIp(req);
+        if (!ip) return req.ip || "127.0.0.1";
+        return ip;
+    },
+    message: tooManyRequestsHandler,
+});
+
+export { urlGenerationRateLimit, urlRedirectRateLimit };
