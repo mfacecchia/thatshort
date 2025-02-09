69b98d9c9fbd7f434d9fc52bc0ae6ba44109283e
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-09 12:18:03 +0100

    feat: add custom AppError handling

--- backend/src/common/middlewares/handleError.ts
+++ backend/src/common/middlewares/handleError.ts
@@ -1,15 +1,24 @@
 import { ErrorRequestHandler } from "express";
-import TResponse from "../types/response.type";
+import AppError from "../errors/AppError";
+import TResponse, { TDefaultResponseBody } from "../types/response.type";
 import { setJsonResponse } from "../utils/setJsonResponse";
 
 const handleError: ErrorRequestHandler = (err, req, res: TResponse, next) => {
     logError(err);
-    // TODO: Handle custom AppError as well
+    let jsonResponse: TDefaultResponseBody = {
+        status: 400,
+        message: "An unexpected error occured. Please try again later.",
+        data: {},
+    };
+    if (err instanceof AppError) {
+        jsonResponse.status = err.statusCode;
+        jsonResponse.message = err.message;
+    }
     setJsonResponse(
         res,
-        400,
-        "An unexpected error occured. Please try again later.",
-        {}
+        jsonResponse.status,
+        jsonResponse.message,
+        jsonResponse.data
     );
     return;
 };
